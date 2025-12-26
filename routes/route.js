7bfa205679e52544f97f6c3b7f9267bd560f85d0
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2025-12-27 00:48:57 +0800

    db name mismatch ://

--- routes/route.js
+++ routes/route.js
@@ -29,7 +29,7 @@ router.get('/:shortCode', async (req, res) => {
     const { shortCode } = req.params;
 
     try {
-        const result = await pool.query('SELECT long_url FROM urls WHERE short_code = $1', [shortCode]);
+        const result = await pool.query('SELECT long_url FROM urls WHERE shorturl = $1', [shortCode]);
         if (result.rows.length === 0) {
             return res.status(404).json({ error: 'Short URL not found' });
         }
